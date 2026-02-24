de64c044c7383a55288f91c33380381553c17c39
Kitround <github@krnd.fr>
2026-02-24 15:56:29 +0100

    Initial Commit

---
+++ MediaKeys/MediaKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaKeysApp.swift
+//  MediaKeys
+//
+//  Created by kmac on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MediaKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
